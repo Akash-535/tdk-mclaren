8d5fafc816050e64a05f53494bc4cf415b166cd4
akash-535 <akashbish9e@gmail.com>
2024-12-13 16:30:51 +0530

    hero height added

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div className='bg-hero-bg bg-bottom bg-no-repeat bg-cover pt-20 min-h-[78.43vh] max-xl:min-h-screen max-xl:bg-right max-lg:bg-center max-lg:min-h-[78.43vh] max-md:pt-12 max-sm:pt-[39px]'>
+        <div className='bg-hero-bg bg-bottom bg-no-repeat bg-cover pt-20 min-h-[82.43vh] max-xl:min-h-screen max-xl:bg-right max-lg:bg-center max-lg:min-h-[78.43vh] max-md:pt-12 max-sm:pt-[39px]'>
             <div className="container text-center">
                 <h1 className='text-custom-4xl text-white font-bold leading-custom-2xl max-xl:max-w-[753px] mx-auto max-md:text-5xl max-sm:text-3xl max-sm:max-w-[374px]'>
                     Driving Transformation for Performance and Passion Within
