4d5dbd95c3a91a75fb4a21b88e17311bef8666e2
akash-535 <akashbish9e@gmail.com>
2024-12-14 09:44:28 +0530

    folder updated

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../components/home/Hero'
 import Header from '../common/Header'
-import Partner from '../components/partners/Partner'
+import Partner from '../components/home/Partner'
 import MySwiper from '../components/home/MySwiper'
 
 const Home = () => {
